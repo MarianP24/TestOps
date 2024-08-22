bf6d06b66c4811ae50b83cffbb9467f77df01978
ParaianuMarian <p.marian24@yahoo.com>
2024-08-22 16:32:39 +0300

    http://localhost:8080/ works with a link on Options-FixtureController

---
+++ src/main/java/com/hella/ictmanager/controller/FixtureThymeleafController.java
@@ -0,0 +1,85 @@
+package com.hella.ictmanager.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/fixturesThymeleaf")
+public class FixtureThymeleafController {
+
+    // Endpoints
+    private static final String SAVE_FIXTURE = "Insert fixture";
+    private static final String GET_FIXTURE_BY_ID = "Find fixture by ID";
+    private static final String GET_FIXTURES = "List fixtures";
+    private static final String UPDATE_FIXTURE = "Update fixture by ID ";
+    private static final String DELETE_FIXTURE = "Delete fixture by ID";
+    private static final String ADD_FIXTURE_TO_MACHINE = "Add fixture to machine";
+    private static final String CREATE_MAINTENANCE_REPORT = "Create maintenance report";
+
+    @GetMapping
+    public String listEndpoints(Model model) {
+        model.addAttribute("saveFixture", SAVE_FIXTURE);
+        model.addAttribute("FindFixtureById", GET_FIXTURE_BY_ID);
+        model.addAttribute("listFixtures", GET_FIXTURES);
+        model.addAttribute("updateFixturebyID", UPDATE_FIXTURE);
+        model.addAttribute("deleteFixturebyID", DELETE_FIXTURE);
+        model.addAttribute("addFixtureToMachine", ADD_FIXTURE_TO_MACHINE);
+        model.addAttribute("createMaintenanceReport", CREATE_MAINTENANCE_REPORT);
+        return "index"; // Șablonul Thymeleaf pentru listarea fixture-urilor
+    }
+
+    @GetMapping("/listEndpointsController")
+    public String showFixtureControllerPage(Model model) {
+        model.addAttribute("saveFixture", SAVE_FIXTURE);
+        model.addAttribute("findFixtureById", GET_FIXTURE_BY_ID);
+        model.addAttribute("listFixtures", GET_FIXTURES);
+        model.addAttribute("updateFixturebyID", UPDATE_FIXTURE);
+        model.addAttribute("deleteFixturebyID", DELETE_FIXTURE);
+        model.addAttribute("addFixtureToMachine", ADD_FIXTURE_TO_MACHINE);
+        model.addAttribute("createMaintenanceReport", CREATE_MAINTENANCE_REPORT);
+        return "fixtureControllerForms/listEndpointsController"; // Afișează pagina cu lista de stringuri
+    }
+
+    @PostMapping("/save fixture")
+    public String saveFixtureForm(Model model) {
+        model.addAttribute("action", SAVE_FIXTURE);
+        return "fixtureControllerForms/saveFixtures"; // Afișează formularul de creare a unui fixture
+    }
+
+    @GetMapping("/get fixture by ID")
+    public String findFixtureByIdForm() {
+        // Logica pentru crearea fixture-ului
+        return "fixtureControllerForms/getFixtureById"; // Afișează formularul de căutare a unui fixture
+    }
+
+    @GetMapping("/list fixtures")
+    public String listFixturesForm(Model model) {
+        model.addAttribute("action", GET_FIXTURES);
+        return "fixtureControllerForms/listFixtures"; // Afișează formularul cu fixture-urile existente
+    }
+
+    @PutMapping("/update fixture by ID")
+    public String updateFixtureForm() {
+        // Logica pentru actualizarea fixture-ului
+        return "fixtureControllerForms/updateFixture"; // Afișează formularul de actualizare a unui fixture
+    }
+
+    @DeleteMapping("/delete fixture by ID")
+    public String deleteFixtureForm() {
+        // Logica pentru ștergerea fixture-ului
+        return "fixtureControllerForms/deleteFixtureByID"; // Afișează formularul de ștergere a unui fixture dupa ID
+    }
+
+    @PostMapping("/add fixture to machine")
+    public String addFixtureToMachineForm() {
+        // Logica pentru adăugarea fixture-ului la o mașină
+        return "fixtureControllerForms/addFixtureToMachine"; // Afișează formularul de adăugare a unui fixture la o mașină
+    }
+
+    @PostMapping("/create maintenance report")
+    public String createMaintenanceReportForm() {
+        // Logica pentru crearea raportului de mentenanță
+        return "fixtureControllerForms/createMaintenanceReport"; // Afișează formularul de creare a unui raport de mentenanță
+    }
+}
